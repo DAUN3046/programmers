3d37216fdb1bf04f9a66d307fc0a2be52db1faa5
Daun Kim <>
2021-05-04 13:59:23 +0000

    2021-05-04

--- 코딩테스트 연습/level 1/K번째 수.py
+++ 코딩테스트 연습/level 1/K번째 수.py
@@ -5,12 +5,12 @@ def solution(array, commands):
     idx = 0
     for row in range(len(commands)):
         newArray = array[commands[row][0] - 1:commands[row][1]]
-        print("newArray:",newArray)
+        # print("newArray:",newArray)
         newArray2 = sorted(newArray)
-        print("newArray2:", newArray2)
+        # print("newArray2:", newArray2)
         idx = commands[row][2]
-        print("idx:", idx)
-        print("newArray2[idx]:", newArray2[idx - 1])
+        # print("idx:", idx)
+        # print("newArray2[idx]:", newArray2[idx - 1])
         answer.append(newArray2[idx - 1])
-        print(answer)
+        # print(answer)
     return answer
