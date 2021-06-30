a589cbde1245904d1eaced71b6dadb637d7d43da
daun kim <49031232+DAUN3046@users.noreply.github.com>
2021-06-30 13:15:36 +0000

    2021-06-30

--- 코딩테스트 연습/level 2/가장 큰 수.py
+++ 코딩테스트 연습/level 2/가장 큰 수.py
@@ -2,12 +2,26 @@
 # 100,000(길이)x1,000(원소 자리수) 배열에 하나하나 넣어볼까?
 # 내림차순 정렬 후 [i][j] 반복문 돌려서 i마다 큰 거 뽑고, 숫자가 같으면 길이 비교 -  길면 기차 ..........
 # 근데 리스트 크기가 억이면 for문 한 번만 들어가도 시간 초과인디... 생각해보자 생각
-# [[6],
-#  [1, 0],
-#  [2]]
+
+# 자릿수가 짧은 순으로 내림차순 정렬하되 다른 숫자들보다 큰 자릿수부터 비교했을 때 커야됨 
 def solution(numbers):
     answer = ''
-    array = [[0]*100000 for _ in range(1000)
+    numbers1 = [] # 정렬된 리스트
+    cnt = 0
+    str1, str2 = '', ''
+    numbers1 = numbers
+    numbers1 = sorted(list(map(str, numbers)), key = lambda x: x*10, reverse = True)
+    # print(numbers1)
+    
+    # answer 채움
+    for i in numbers1:
+        answer += str(i)
+        
+    # numbers가 0으로만 이루어진 경우
+    if int(answer) == 0:
+        answer = '0'
+    
+    # print(answer)
     return answer
 
 
